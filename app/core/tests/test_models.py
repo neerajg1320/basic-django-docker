279ad5b0f7de9b1e7bb3a1d651dc26d4080fec98
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2022-04-27 22:47:51 +0530

    Added custom user model

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+def sample_user(email='test@abc.com', password='Test123'):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email, password)
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successful"""
+        email = 'test@abc.com'
+        password = 'Test123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
--- run.cmd
+++ run.cmd
@@ -1,3 +1,9 @@
 docker build .
 
 docker compose run app sh -c "django-admin startproject app ."
+docker compose run app sh -c "python manage.py startapp core"
+
+docker compose run app sh -c "python manage.py test"
+
+# After creating custom user
+docker compose run app sh -c "python manage.py makemigrations core"
